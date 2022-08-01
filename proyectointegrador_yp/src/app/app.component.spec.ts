f221318ce4ea7df0a9118145130851cd6e0caa2f
yesicapesqueira20 <110070292+yesicapesqueira20@users.noreply.github.com>
2022-07-31 22:15:47 -0300

    hacer de nuevo el heaDER

--- proyectointegrador_yp/src/app/app.component.spec.ts
+++ proyectointegrador_yp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyectointegrador-yp'`, () => {
+  it(`should have as title 'proyectointegrador_yp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectointegrador-yp');
+    expect(app.title).toEqual('proyectointegrador_yp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyectointegrador-yp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proyectointegrador_yp app is running!');
   });
 });
--- proyectointegrador_yp/src/app/app.component.ts
+++ proyectointegrador_yp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectointegrador-yp';
+  title = 'proyectointegrador_yp';
 }
